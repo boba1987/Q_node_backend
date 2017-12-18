e78605900278ea067207456ecc88fb4521360197
Slobodan Djordjevic <sdjordjevic@razor.rs>
2017-12-18 20:19:00 +0100

    update and find function for mongo db

--- mongo/index.js
+++ mongo/index.js
@@ -4,26 +4,60 @@ const config = require('../config.json');
 
 // Connection URL
 const url = config.mongo;
+let dbInstance;
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  // Get the collection
+  dbInstance = db;
+});
 
 // Inset into database
 function insert(data, dbCollection, callback) {
-  // Use connect method to connect to the Server
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    // Get the collection
-    const collection = db.collection(dbCollection);
-    // Insert provided document/s
-    collection.insertMany([
-      data
-    ], function(err, result) {
-      if (callback) {
-        callback(result);
-      }
-      db.close();
-    });
+  const collection = dbInstance.collection(dbCollection);
+  // Insert provided document/s
+  collection.insertMany([
+    data
+  ], function(err, result) {
+    if (callback) {
+      callback(result);
+    }
+  });
+}
+
+// Update database entry
+function update(filter, data, dbCollection, callback) {
+  const collection = dbInstance.collection(dbCollection);
+  // Update provided document/s
+  collection.updateMany(filter, data, function(err, docs) {
+    if (err) {
+      console.log('Find error: ', err);
+    }
+
+    if (callback) {
+      callback(docs);
+    }
+  });
+}
+
+// Search for documents in db
+function find(filter = {}, dbCollection, callback) {
+  // Get the documents collection
+  var collection = dbInstance.collection(dbCollection);
+  // Find some documents
+  collection.find(filter).toArray(function(err, docs) {
+    if (err) {
+      console.log('Find error: ', err);
+    }
+    if (callback) {
+      callback(docs);
+    }
   });
 }
 
 module.exports = {
-  insert
+  insert,
+  update,
+  find
 }
