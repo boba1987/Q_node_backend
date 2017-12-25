d7e8c97a5ab0086ca91977214e9aa8f02dd59a96
Slobodan Djordjevic <sdjordjevic@razor.rs>
2017-12-25 14:39:51 +0100

    Remove res object from validation - redundant

--- validator/index.js
+++ validator/index.js
@@ -1,7 +1,7 @@
 const validate = require('jsonschema').Validator;
 const validator = new validate();
 
-function isValid(req, res, schema) {
+function isValid(req, schema) {
   let validation = validator.validate(req.body, schema).errors;
   if(validation.length != 0){ // Validate body of the request
     for (let i=0; i<validation.length; i++) {
