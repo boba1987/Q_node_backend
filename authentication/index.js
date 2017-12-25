9a8eb1050b850ff09d531791bfb64fd5e79c0536
Slobodan Djordjevic <sdjordjevic@razor.rs>
2017-12-25 16:28:48 +0100

    Extract /login route into a separate module

--- authentication/index.js
+++ authentication/index.js
@@ -1,6 +1,11 @@
 const passportJWT = require('passport-jwt');
 const secretKey = require('../secrets/privateKey.json');
 const mongo = require('../mongo/index.js');
+const config = require('../config.json');
+const jwt = require('jsonwebtoken');
+const loginSchema = require('../schemas/login.json');
+const validator = require('../validator');
+const Q = require('Q');
 
 const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
@@ -21,7 +26,41 @@ const strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
   });
 });
 
+function login(req) {
+  const deferred = Q.defer();
+  const v = validator.isValid(req, loginSchema);
+
+  if (v) {
+    deferred.reject({message: v, status: 400});
+  } else {
+    // Get the user
+    mongo.findOne({name: req.body.name}, {fields: {_id: 0, id: 1, name: 1, password: 1}}, 'users', function(user) {
+      if( ! user ){
+        deferred.reject({message: 'User name or password does not match', status: 401});
+      }
+
+      if(user.password === req.body.password) {
+        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+        var token = jwt.sign(user, secretKey.key);
+        // Save token to the token store
+        const tokenObj = {token, time: new Date().getTime(), user: user._id, expiration: new Date().getTime() + config.tokenExpiration};
+        mongo.insert(tokenObj, 'token_store' );
+        // Atthach token to a user
+        mongo.update({name: user.name}, {$set: {auth: tokenObj}}, 'users', function(){
+          mongo.findOne({name: req.body.name}, {fields: {_id: 1, name: 1, number: 1, role: 1, 'auth.token': 1}}, 'users', function(user) {
+            deferred.resolve(user);
+          });
+        })
+      } else {
+        return deferred.reject({message: 'User name or password does not match', status: 401});
+      }
+    });
+  }
+
+  return deferred.promise;
+}
+
 module.exports = {
   strategy,
-  jwtOptions
+  login
 };
--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const config = require('./config.json');
 const appPort = 3000;
 
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
 
-const passportSettings = require('./authentication');
+const authentication = require('./authentication');
 const mongo = require('./mongo');
 const resolver = require('./resolver');
 const userManagement = require('./user_management');
 const messages = require('./messages');
 
-const loginSchema = require('./schemas/login.json');
-const validator = require('./validator');
 
-passport.use(passportSettings.strategy);
+passport.use(authentication.strategy);
 
 app
   .use(passport.initialize())
@@ -70,39 +66,16 @@ app
 // POST routes
 app
   .post('/login', function(req, res) {
-    const v = validator.isValid(req, res, loginSchema);
-
-    if (v) {
-      return res.status(400).json({message: v});
-    }
-
-    // Get the user
-    mongo.findOne({name: req.body.name}, {fields: {_id: 0, id: 1, name: 1, password: 1}}, 'users', function(user) {
-      if( ! user ){
-        res.status(401).json({message: 'User name or password does not match'});
-      }
-
-      if(user.password === req.body.password) {
-        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-        var token = jwt.sign(user, passportSettings.jwtOptions.secretOrKey);
-        // Save token to the token store
-        const tokenObj = {token, time: new Date().getTime(), user: user._id, expiration: new Date().getTime() + config.tokenExpiration};
-        mongo.insert(tokenObj, 'token_store' );
-        // Atthach token to a user
-        mongo.update({name: user.name}, {$set: {auth: tokenObj}}, 'users', function(){
-          mongo.findOne({name: req.body.name}, {fields: {_id: 1, name: 1, number: 1, role: 1, 'auth.token': 1}}, 'users', function(user) {
-            res.json(user);
-          });
-        })
-      } else {
-        res.status(401).json({message: 'User name or password does not match'});
-      }
+    authentication.login(req).then(user => {
+      res.json(user);
+    }).catch(err => {
+      res.status(err.status).send(err.message);
     });
   })
   .post('/createUser', passport.authenticate('jwt', {session: false}), function(req, res) {
     userManagement.create(req, res).then(() => {
       res.sendStatus(200);
     }).catch(err => {
-      res.status(400).json(err);
+      res.status(400).json(err.message);
     });
   })
