141b4af86a6ef4c8b6e935d3da1641a69beff28b
Slobodan Djordjevic <sdjordjevic@razor.rs>
2017-12-27 17:26:12 +0100

    Generate random hash util function

---
+++ utils/index.js
@@ -0,0 +1,14 @@
+// Genetate random string
+function makeRandomHash(length) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+module.exports = {
+  makeRandomHash
+}
