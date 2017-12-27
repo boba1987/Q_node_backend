5665a952af02b589973e543743638f9327707a97
Slobodan Djordjevic <sdjordjevic@razor.rs>
2017-12-27 11:21:34 +0100

    Mongo seed update, "responses from" revert in messages queue

--- messages/index.js
+++ messages/index.js
@@ -9,7 +9,7 @@ function getMessages(req) {
       messages.items.map(message => { // Enhance each messsage with number of subscribers
         message.responseFrom = [];
         queues.map(queue => { // Map trough queues to attahch subscribers to response and attach whoever replied
-          if (message.queue == queue.name) {
+          if (message.queue == queue.queue) {
             message.subscribers = queue.subscribers;
             queue.responseFrom.map(replied => { // Attach whoever replied
               if (message.responseFrom.indexOf(replied) == -1 ) {
@@ -19,9 +19,9 @@ function getMessages(req) {
           }
         })
       })
-    })
 
-    deferred.resolve(messages)
+      deferred.resolve(messages);
+    });
   });
 
   return deferred.promise;
