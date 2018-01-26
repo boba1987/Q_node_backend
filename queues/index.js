8ee5b83623d88fcc2089c95d4101f0cd54001f75
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2018-01-26 18:32:37 +0100

    Queue creation fix

--- queues/index.js
+++ queues/index.js
@@ -7,6 +7,11 @@ const formidable = require('formidable');
 const parser = require('../parser');
 const fs = require('fs');
 
+module.exports = {
+  editStatus,
+  create
+}
+
 function editStatus(req) {
   const deferred = q.defer();
   const v = validator.isValid(req, queuesSchema.editStatus); // Validate request
@@ -51,47 +56,42 @@ function create(req) {
         queue.push(parser.csv(files[key].path));
         // Remove file from ./tmp
         fs.unlinkSync('./' + files[key].path);
-
-        q.all(
-          queue
-        ).then(parsed => {
-          let index = 0;
-
-          for (let key in files) {
-            fields[key] = parsed[index]; // Enhance fields object with allowedNumbersToSend/allowedNumbersToSubscribe
-            index++;
-          }
-
-          // Enhabce fields object with arrays to contain status, responseFrom and subscribers
-          fields.responseFrom = [];
-          fields.subscribers = [];
-          fields.active = false;
-          fields.time = new Date();
-
-          if (typeof fields.allowedNumbersToSend == 'string') {
-            fields.allowedNumbersToSend = fields.allowedNumbersToSend.split(',').map(function(item) {
-              return item.trim();
-            });
-          }
-
-          if (typeof fields.allowedNumbersToSubscribe == 'string') {
-            fields.allowedNumbersToSubscribe = fields.allowedNumbersToSubscribe.split(',').map(function(item) {
-              return item.trim();
-            });
-          }
-        }).catch( err =>{
-          deferred.reject({status: err.status, message: err.message});
-        });
       }
     }
 
-    save(fields, deferred);
+    q.all(
+      queue
+    ).then(parsed => {
+      let index = 0;
+
+      for (let key in files) {
+        fields[key] = parsed[index]; // Enhance fields object with allowedNumbersToSend/allowedNumbersToSubscribe
+        index++;
+      }
+
+      // Enhance fields object with arrays to contain status, responseFrom and subscribers
+      fields.responseFrom = [];
+      fields.subscribers = [];
+      fields.active = false;
+      fields.time = new Date();
+
+      if (typeof fields.allowedNumbersToSend == 'string') {
+        fields.allowedNumbersToSend = fields.allowedNumbersToSend.split(',').map(function(item) {
+          return item.trim();
+        });
+      }
+
+      if (typeof fields.allowedNumbersToSubscribe == 'string') {
+        fields.allowedNumbersToSubscribe = fields.allowedNumbersToSubscribe.split(',').map(function(item) {
+          return item.trim();
+        });
+      }
+
+      save(fields, deferred);
+    }).catch( err =>{
+      deferred.reject({status: err.status, message: err.message});
+    });
   });
 
   return deferred.promise;
-}
-
-module.exports = {
-  editStatus,
-  create
 }
