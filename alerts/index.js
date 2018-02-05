b95a66b7f7c5856b41f46706fd5c5fcd3944f471
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2018-02-05 03:30:53 +0100

    Send alert message on click

--- alerts/index.js
+++ alerts/index.js
@@ -1,6 +1,9 @@
 const q = require('q');
 const validator = require('../validator');
 const alertsSchema = require('../schemas/alerts.json');
+const bot = require('../bot');
+const mongo = require('../mongo');
+const MongoDB = require('mongodb');
 
 function sendMail(req) {
   const deferred = q.defer();
@@ -24,9 +27,22 @@ function sendSms(req) {
   if (v) {
     deferred.reject({status: 400, message: v});
   } else {
-    // TODO: implement mail sender
-    console.log('sendSms', req.body);
-    deferred.resolve();
+    // Find a queue
+    mongo.findOne({queueType: req.body.queue}, {}, 'queues', (queue) => {
+      // If queue not found
+      if (!queue) {
+        console.log(req.body.queue + 'Queue not found!');
+      }
+
+      // Find a message that created the alert
+      mongo.findOne({_id: new MongoDB.ObjectID(req.body._id)}, {}, 'alerts', (doc) => {
+        bot.sendMessage({
+          numbers: queue.owner,
+          message: 'Message "' + doc.message + '" sent by ' + doc.sender + ' is sent to an empty ' + req.body.queue + ' queue'
+        });
+      })
+      deferred.resolve();
+    })
   }
 
   return deferred.promise;
