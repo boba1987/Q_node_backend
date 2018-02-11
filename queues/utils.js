64c271468574efcbde62f4fa9a31ee416a14e659
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2018-02-11 11:55:09 +0100

    PA object

---
+++ queues/utils.js
@@ -0,0 +1,36 @@
+module.exports = {
+  PAobject
+};
+
+const PAobject = [
+    {
+        'id': '1',
+        'input': 'false',
+        'style': {
+            'bg_color': ''
+        },
+        'rows': [
+            {
+                'style': {
+                    'size': '1.00',
+                    'bg_color': ''
+                },
+                'cells': [
+                    {
+                        'title' : 'Show an info',
+                        'cmd'   : '/showinfo',
+                        'input' : 'false',
+                        'link'  : '1',
+                        'echo'  : 'true',
+                        'style': {
+                            'color'   : '#ffffff',
+                            'border'  : '#999999',
+                            'bg_color': '#999999',
+                            'width'   : '1'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+]
