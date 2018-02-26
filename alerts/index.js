ca6ecf0cf762909a89252dd3fb4d2f1ecda5c171
Slobodan Djordjevic <boba_dj1987@hotmail.com>
2018-02-26 03:59:10 +0100

    Send mail alert

--- alerts/index.js
+++ alerts/index.js
@@ -6,6 +6,7 @@ const mongo = require('../mongo');
 const MongoDB = require('mongodb');
 const nodemailer = require('nodemailer');
 const emailValidator = require('email-validator');
+const colors = require('colors');
 
 const alertActions = {
     2: AlertQueueOwnerByMessage
@@ -183,42 +184,50 @@ function sendMail(req) {
     deferred.reject({status: 400, message: v});
   } else {
     // Find a queue
-    mongo.findOne({queueType: req.body.queue}, {}, 'queues', (queue) => {
+    let queueType = req.body.queue.substr(0, req.body.queue.indexOf('_'));
+    mongo.findOne({queueType: queueType}, {}, 'queues', (queue) => {
       // If queue not found
       if (!queue) {
         console.log(req.body.queue + 'Queue not found!');
       }
 
-      // Find a message that created the alert
-      mongo.findOne({_id: new MongoDB.ObjectID(req.body._id)}, {}, 'alerts', (doc) => {
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: 'medxq.info@gmail.com',
-                pass: 'medxqapp'
-            }
-        });
+      // Check if mail is set
+      if (!emailValidator.validate(queue.queueEscalation)) {
+          console.log(colors.red('Email is not set as escalation option'));
 
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: '"MedxQ Info" <medxq.info@gmail.com>', // sender address
-            to: 'sdjordjevic@razor.rs', // list of receivers
-            subject: 'Queue Alert', // Subject line
-            text: 'Message: "' + doc.message + '", sent by ' + doc.sender + '. Alert: ' + req.body.alert,
-            html: '<b>Message: </b>"' + doc.message + '"<br/> <b>sent by</b>' + doc.sender + ' <br/> <b>Alert: </b>' + req.body.alert// html body
-        };
+          deferred.resolve();
+      } else {
+          // Find a message that created the alert
+          mongo.findOne({_id: new MongoDB.ObjectID(req.body._id)}, {}, 'alerts', (doc) => {
+              let transporter = nodemailer.createTransport({
+                  service: 'gmail',
+                  port: 587,
+                  secure: false, // true for 465, false for other ports
+                  auth: {
+                      user: 'medxq.info@gmail.com',
+                      pass: 'medxqapp'
+                  }
+              });
 
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error) => {
-            if (error) {
-                return console.log('transporter.sendMail: ', error);
-            }
+              // setup email data with unicode symbols
+              let mailOptions = {
+                  from: '"MedxQ Info" <medxq.info@gmail.com>', // sender address
+                  to: queue.queueEscalation, // list of receivers
+                  subject: 'Queue Alert', // Subject line
+                  text: 'Message: "' + doc.message + '", sent by ' + doc.sender + '. Alert: ' + req.body.alert,
+                  html: '<b>Message: </b>"' + doc.message + '"<br/> <b>sent by</b>' + doc.sender + ' <br/> <b>Alert: </b>' + req.body.alert// html body
+              };
 
-            deferred.resolve();
-        });
-      })
+              // send mail with defined transport object
+              transporter.sendMail(mailOptions, (error) => {
+                  if (error) {
+                      return console.log('transporter.sendMail: ', error);
+                  }
+
+                  deferred.resolve();
+              });
+          })
+      }
     })
   }
 
